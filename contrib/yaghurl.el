aa00b8bbd278538d223600d3fc08db97d53a7fe8
Nathan Wilcox <nejucomo@gmail.com>
2015-10-18 18:41:35 -0700

    Improve a doc-string's accuracy in the emacs mode.

--- contrib/yaghurl.el
+++ contrib/yaghurl.el
@@ -1,6 +1,6 @@
 
 (defun yaghurl-current-file ()
-  "Copy yaghurl output to the clipboard for current file/line."
+  "Copy yaghurl output to the clipboard for current file."
   (let* ((filename (buffer-file-name)))
     (call-process-shell-command (concat "yaghurl -o xclip " filename))))
 
